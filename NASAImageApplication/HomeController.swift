883d6f518495c81ccb8d9068757dc222b4651bd4
Christopher Vensand <chrisvensand@gmail.com>
2018-07-21 12:06:50 -0700

    Initial commit

---
+++ NASAImageApplication/HomeController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  NASAImageApplication
+//
+//  Created by Christopher Vensand on 7/21/18.
+//  Copyright © 2018 Christopher Vensand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor().cyan
+        
+    }
+    
+    
+
+}
+
