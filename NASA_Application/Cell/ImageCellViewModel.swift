1b4f49e70b371364c695e0af5780edb7e98ad2c2
cvensand <cvensand@umich.edu>
2019-01-16 21:07:01 -0500

    setImage method updated to fetch url data

--- NASA_Application/Cell/ImageCellViewModel.swift
+++ NASA_Application/Cell/ImageCellViewModel.swift
@@ -10,7 +10,27 @@ import UIKit
 
 final class ImageTableViewCell: UITableViewCell {
     
-    var cellImage: UIImage? = UIImage(named: "defaultImage")
+    lazy var imgView = UIImageView()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.contentView.addSubview(imgView)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.imgView.translatesAutoresizingMaskIntoConstraints = false
+        self.imgView.contentMode = .scaleAspectFill
+        
+        NSLayoutConstraint.activate([
+                                    imgView.leftAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leftAnchor, constant: 10.0),
+                                    imgView.rightAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.rightAnchor, constant: 10.0),
+                                    imgView.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 10.0),
+                                    imgView.bottomAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.bottomAnchor, constant: 10.0)
+                                    ])
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     // MARK: - Helpers
     
@@ -21,33 +41,39 @@ final class ImageTableViewCell: UITableViewCell {
             return
         }
         
-        let request = URLRequest(url: url)
-        
-        fetchImage(request)
+        DispatchQueue.global(qos: .userInitiated).async {
+            guard let data = try? Data(contentsOf: url) else {
+                return
+            }
+            
+            DispatchQueue.main.async { [weak self] in
+                self?.imgView.image = UIImage(data: data)
+            }
+        }
         
     }
     
-    private func fetchImage(_ request: URLRequest) {
-        
-        URLSession.shared.dataTask(with: request) { (data, response, error) in
-            guard let data = data else {
-                if let error = error {
-                    print("error \(error.localizedDescription)")
-                } else {
-                    print("Unknown error")
-                }
-                return
-            }
-            
-            guard let cellImage = UIImage(data: data) else {
-                print("Unable to create UIImage")
-                return
-            }
-            
-            DispatchQueue.main.async {
-                self.cellImage = cellImage
-            }
-        }.resume()
-    }
+//    private func fetchImage(_ request: URLRequest) {
+//
+//        URLSession.shared.dataTask(with: request) { (data, response, error) in
+//            guard let data = data else {
+//                if let error = error {
+//                    print("error \(error.localizedDescription)")
+//                } else {
+//                    print("Unknown error")
+//                }
+//                return
+//            }
+//            
+//            guard let cellImage = UIImage(data: data) else {
+//                print("Unable to create UIImage")
+//                return
+//            }
+//
+//            DispatchQueue.main.async {
+//                self.cellImage = cellImage
+//            }
+//        }.resume()
+//    }
     
 }
